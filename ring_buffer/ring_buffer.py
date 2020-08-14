a83ca175a9b67cfb3ff71894ac23884c827e6aae
Tusk617 <59673124+Tusk617@users.noreply.github.com>
2020-08-14 11:33:17 -0400

    Got a semi working implementation of ring buffer, seems to be failing on 2 tests for some reason though

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,21 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        # pass
+        from collections import deque
+        self.capacity = capacity
+        self.stack = []
+        self.data = deque([], maxlen=capacity)
 
     def append(self, item):
-        pass
+        # pass
+        self.data.append(item)
+        if self.data.index == 3:
+            self.data.appendleft(item)
 
     def get(self):
-        pass
+        # pass
+        for i in self.data:
+            self.stack.append(i)
+        return self.stack
+
+
